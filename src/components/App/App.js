f82d10741956a55a79d504fcf79076104ca7f41d
Sergey Penkin <p3nkinn@gmail.com>
2022-10-04 16:52:11 +0300

    level-2

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import Header from '../Header/Header.js';
+import Promo from '../Promo/Promo';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Promo />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import moviesLogo from "../../images/head_logo.svg";
+import "./Header.css";
+function Header() {
+  return (
+    <header className="header">
+      <a className="header__link" href="about">
+        <img src={moviesLogo} className="header__logo" alt="логотип" />
+      </a>
+      <div className="header__user">
+        <a className="header__auth-register" href="sign-in">
+          Регистрация
+        </a>
+        <button type="button" className="header__auth-login" href="sign-up">
+        Войти
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
