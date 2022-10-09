082d7a18439acf5f74babe38e0bd2153efc9251c
Sergey Penkin <p3nkinn@gmail.com>
2022-10-09 18:55:27 +0300

    level-2

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,14 +1,42 @@
 import React from "react";
-import '../AboutProject/AboutProject.css';
+import "../AboutProject/AboutProject.css";
 
 function AboutProject() {
-    return (
-        <section className="about">
-            <div className="main-content">
-                <h2 id="about" className="about__title">О проекте</h2>
-            </div>
-        </section>
-    )
+  return (
+    <section className="about">
+      <div className="main-content">
+        <h2 id="about" className="about__title">
+          О проекте
+        </h2>
+        <div className="about__wrapper">
+          <div className="about__project">
+            <h2 className="about__info">Дипломный проект включал 5 этапов</h2>
+            <p className="about__descr">
+              Составление плана, работу над бэкендом, вёрстку, добавление
+              функциональности и финальные доработки.
+            </p>
+          </div>
+          <div className="about__project">
+            <h2 className="about__info">На выполнение диплома ушло 5 недель</h2>
+            <p className="about__descr">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+              соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+        <div className="about__jobwork">
+        <div className="about__greenweek">
+            <h3 className="about__oneweek">1 неделя</h3>
+            <p className="about__subweek">Back-end</p>
+          </div>
+          <div className="about__greyweek">
+            <h3 className="about__oneweek">4 недели</h3>
+            <p className="about__subweek">Front-end</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,8 @@ import Promo from "../Promo/Promo";
 import NavTab from "../NavTab/NavTab";
 import "../Main/Main.css";
 import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 function Main() {
   return (
@@ -10,6 +12,8 @@ function Main() {
       <Promo />
       <NavTab />
       <AboutProject />
+      <Techs />
+      <AboutMe />
     </>
   );
 }
