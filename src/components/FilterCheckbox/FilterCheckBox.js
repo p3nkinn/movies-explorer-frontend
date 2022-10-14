0b5f311561ae699bba755c9c029df78cde520b14
Sergey Penkin <p3nkinn@gmail.com>
2022-10-14 21:56:22 +0300

    level-2

---
+++ src/components/FilterCheckbox/FilterCheckBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../FilterCheckbox/FilterCheckBox.css';
+
+function FilterCheckBox() {
+    return (
+        <div className="filter__checkbox">
+             <input className="filter__checkbox-switch" type="checkbox"></input>
+        </div>
+    )
+}
+
+export default FilterCheckBox;
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,20 +1,25 @@
 import React from "react";
 import "../SearchForm/SearchForm.css";
 import searchImage from "../../images/search.png";
+import FilterCheckBox from "../FilterCheckbox/FilterCheckBox";
 
 function SearchForm() {
   return (
     <section className="search">
       <div className="main-content">
-        <div className="search__container">
           <form name="searchName" action="#" className="search__form">
-            <input className="search__input">
+            <input placeholder="Фильм" type="text" className="search__input">
             </input>
             <button type="submit" className="search__button">
                 <img src={searchImage} className="form__img" alt="поиск"></img>
               </button>
+              <div className="search__checkbox">
+                <p className="search__short">
+                Короткометражки
+                </p>
+                <FilterCheckBox />
+              </div>
           </form>
-        </div>
       </div>
     </section>
   );
